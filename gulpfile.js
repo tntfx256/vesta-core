4cf7a5595749deb3686cadfe1b5c0550cb6130cb
javakh2 <khalilian.javad@gmail.com>
2017-07-01 17:36:11 +0430

    update with new module template

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,23 @@
 const vesta = require('@vesta/devmaid');
 
-const aid = new vesta.TypescriptTarget({
-    targets: ['es5', 'es6'],
+let aid = new vesta.TypescriptTarget({
     genIndex: true,
+    targets: ['es5', 'es6'],
     files: ['.npmignore', 'LICENSE', 'README.md'],
+    publish: '--access=public',
     transform: {
         package: (json, target) => {
             if (target === 'es5') {
-                json.devDependencies.typescript = '2.0';
+                json.devDependencies['typescript'] = '2.0';
+                json.dependencies['es6-promise'] = '^4.1.0';
                 json.devDependencies['@types/es6-promise'] = '^0.0.32';
             }
         },
         tsconfig: (json, target) => {
-            if (target === 'es5') {
-                json.typescript = require('typescript');
-            }
+
+        },
+        module: (target) => {
+            return {'main-module': 'target-module'};
         }
     }
 });
