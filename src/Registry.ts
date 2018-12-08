9bf287854d04ff9909c39aa2d46603ce5d51838b
hbtb <bishehsary@gmail.com>
2018-12-07 22:11:18 -0600

    updating Registry for having private storage

--- src/Registry.ts
+++ src/Registry.ts
@@ -1,17 +1,36 @@
 export class Registry {
 
-    public static set<T>(key: string, value: T) {
+    public static getPrivateStorage(key: string) {
+        if (!(key in Registry.privateStorage)) {
+            Registry.privateStorage[key] = {};
+        }
+
+        return {
+            set: function <T>(key: string, value: T): void {
+                Registry.privateStorage[key] = value;
+            },
+            get: function <T>(key: string, defaultValue?: T): T | null {
+                if (key in Registry.storage) {
+                    return Registry.storage[key];
+                }
+                return defaultValue || null;
+            }
+        }
+    }
+
+    public static set<T>(key: string, value: T): void {
         Registry.storage[key] = value;
     }
 
-    public static get<T>(key: string, defaultValue?: T) {
-        if (key in Registry.storage) {
-            return Registry.storage[key];
+    public static get<T>(key: string, defaultValue?: T): T | null {
+        if (key in Registry.privateStorage) {
+            return Registry.privateStorage[key];
         }
         return defaultValue || null;
     }
 
     private static storage: any = {};
+    private static privateStorage: any = {};
 
     private constructor() { }
 }
